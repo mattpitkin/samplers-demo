d53634af181754f94057c068622b5f31dfad70e3
Matthew Pitkin <matthew.pitkin@ligo.org>
2018-01-31 15:15:24 +0000

    Added standalone PyMC3 example

---
+++ examples/test_PyMC3.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example of running emcee to fit the parameters of a straight line.
+"""
+
+from __future__ import print_function, division
+
+import os
+import sys
+import numpy as np
+
+import matplotlib as mpl
+mpl.use("Agg") # force Matplotlib backend to Agg
+
+# import PyMC3
+import pymc3 as pm
+
+# import model and data
+from createdata import *
+
+# set the PyMC3 model
+linear_model = pm.Model()
+
+with linear_model:
+    # set prior parameters
+    cmin = -10. # lower range of uniform distribution on c
+    cmax = 10.  # upper range of uniform distribution on c
+    
+    mmu = 0.     # mean of Gaussian distribution on m
+    msigma = 10. # standard deviation of Gaussian distribution on m
+    
+    # set priors for unknown parameters
+    cmodel = pm.Uniform('c', lower=cmin, upper=cmax) # uniform prior on y-intercept
+    mmodel = pm.Normal('m', mu=mmu, sd=msigma)       # Gaussian prior on gradient
+    
+    sigmamodel = sigma # set a single standard deviation
+    
+    # Expected value of outcome, aka "the model"
+    mu = mmodel*x + cmodel
+    
+    # Gaussian likelihood (sampling distribution) of observations, "data"
+    Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigmamodel, observed=data)
+
+Nsamples = 1000 # final number of samples
+Ntune = 1000    # number of tuning samples
+
+# perform sampling
+with linear_model:
+    start = pm.find_MAP() # find starting value using optimisation methods
+    trace = pm.sample(Nsamples, start=start, tune=Ntune, discard_tuned_samples=True) # perform sampling
+
+# extract the samples
+postsamples = np.vstack((trace['m'], trace['c'])).T
+
+# plot posterior samples (if corner.py is installed)
+try:
+    import corner # import corner.py
+except ImportError:
+    sys.exit(1)
+
+print('Number of posterior samples is {}'.format(postsamples.shape[0]))
+
+fig = corner.corner(postsamples, labels=[r"$m$", r"$c$"], truths=[m, c])
+fig.savefig('PyMC3.png')
+
