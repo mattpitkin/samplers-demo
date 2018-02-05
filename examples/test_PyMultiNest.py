c9ccb3e0d4de43f0ddeae60075a3f7cdf958a183
Matthew Pitkin <matthew.pitkin@ligo.org>
2018-02-05 23:45:35 +0000

    Added pymultinest example

---
+++ examples/test_PyMultiNest.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example of running CPNest to fit the parameters of a straight line.
+"""
+
+from __future__ import print_function, division
+
+import os
+import sys
+import six
+from scipy.special import ndtri
+import numpy as np
+
+# import PyMultiNest Solver class
+from pymultinest.solve import Solver
+
+# import model and data
+from createdata import *
+
+LN2PI = np.log(2.*np.pi)
+LNSIGMA = np.log(sigma)
+
+# create Solver class
+class StraightLineModelPyMultiNest(Solver):
+    """
+    A simple straight line model, with a Gaussian likelihood.
+
+    Args:
+        data (:class:`numpy.ndarray`): an array containing the observed data
+        abscissa (:class:`numpy.ndarray`): an array containing the points at which the data were taken
+        modelfunc (function): a function defining the model
+        sigma (float): the standard deviation of the noise in the data
+        **kwargs: keyword arguments for the run method
+    """
+
+    # define the prior parameters
+    cmin = -10.  # lower range on c (the same as the uniform c prior lower bound)
+    cmax = 10.   # upper range on c (the same as the uniform c prior upper bound)
+
+    mmu = 0.     # mean of the Gaussian prior on m
+    msigma = 10. # standard deviation of the Gaussian prior on m
+
+    def __init__(self, data, abscissa, modelfunc, sigma, **kwargs):
+        # set the data
+        self._data = data         # oberserved data
+        self._abscissa = abscissa # points at which the observed data are taken
+        self._sigma = sigma       # standard deviation(s) of the data
+        self._logsigma = np.log(sigma) # log sigma here to save computations in the likelihood
+        self._ndata = len(data)   # number of data points
+        self._model = modelfunc   # model function
+
+        Solver.__init__(self, **kwargs)
+
+    def Prior(self, cube):
+        """
+        The prior transform going from the unit hypercube to the true parameters. This function
+        has to be called "Prior".
+
+        Args:
+            cube (:class:`numpy.ndarray`): an array of values drawn from the unit hypercube
+
+        Returns:
+            :class:`numpy.ndarray`: an array of the transformed parameters
+        """
+
+        # extract values
+        mprime = cube[0]
+        cprime = cube[1]
+
+        m = self.mmu + self.msigma*ndtri(mprime)      # convert back to m
+        c = cprime*(self.cmax-self.cmin) + self.cmin  # convert back to c
+
+        return np.array([m, c])
+
+    def LogLikelihood(self, cube):
+        """
+        The log likelihood function. This function has to be called "LogLikelihood".
+
+        Args:
+            cube (:class:`numpy.ndarray`): an array of parameter values.
+
+        Returns:
+            float: the log likelihood value.
+        """
+
+        # extract parameters
+        m = cube[0]
+        c = cube[1]
+
+        # calculate the model
+        model = self._model(x, m, c)
+
+        # normalisation
+        norm = -0.5*self._ndata*LN2PI - self._ndata*self._logsigma
+
+        # chi-squared
+        chisq = np.sum(((self._data - model)/(self._sigma))**2)
+
+        return norm - 0.5*chisq
+
+nlive = 1024 # number of live points
+ndim = 2     # number of parameters
+tol = 0.5    # stopping criterion
+
+# run the algorithm
+solution = StraightLineModelPyMultiNest(data, x, straight_line, sigma, n_dims=ndim,
+                                        n_live_points=nlive, evidence_tolerance=tol);
+
+logZpymnest = solution.logZ        # value of log Z
+logZerrpymnest = solution.logZerr  # estimate of the statistcal uncertainty on logZ
+
+print(six.u('Marginalised evidence is \u00B1 {}'.format(logZpymnest, logZerrpymnest)))
+
+mchain_pymnest = solution.samples[:,0] # extract chain of m values
+cchain_pymnest = solution.samples[:,1] # extract chain if c values
+
+postsamples = np.vstack((mchain_pymnest, cchain_pymnest)).T
+
+print('Number of posterior samples is {}'.format(samples_pymnest.shape[0]))
+
+# plot posterior samples (if corner.py is installed)
+try:
+    import matplotlib as mpl
+    mpl.use("Agg") # force Matplotlib backend to Agg
+    import corner # import corner.py
+except ImportError:
+    sys.exit(1)
+
+fig = corner.corner(postsamples, labels=[r"$m$", r"$c$"], truths=[m, c])
+fig.savefig('PyMultiNest.png')
+
