e58cabf48b3e2a96dcbded70fdfcd30c30040554
Matthew Pitkin <matthew.pitkin@ligo.org>
2018-02-09 10:05:57 +0000

    A variety of edits to display things better

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,13 +32,14 @@ DEFAULT_PAGINATION = 10
 #PLUGINS = ['ipynb.markup']
 
 MARKUP = ['md']
-PLUGIN_PATHS = ['./plugins', './plugins/edited_plugins', './plugins/pelican-plugins']
+#PLUGIN_PATHS = ['./plugins', './plugins/pelican-plugins']
+PLUGIN_PATHS = ['./plugins/pelican-plugins']
 PLUGINS = [
     'summary',       # auto-summarizing articles
     'feed_summary',  # use summaries for RSS, not full articles
-    'ipynb.liquid',  # for embedding notebooks
+    'pelican-ipynb.liquid',  # for embedding notebooks
     'liquid_tags.img',  # embedding images
-    'include_code',  # including code blocks
+    'liquid_tags.include_code',  # including code blocks
     'liquid_tags.literal'
 ]
 IGNORE_FILES = ['.ipynb_checkpoints']
