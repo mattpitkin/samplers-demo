Fix unicode plus/minus sign issue

content/downloads/code/test_DNest4.py
@@ -9,7 +9,6 @@ from __future__ import print_function, division
 
 import os
 import sys
-import six
 import numpy as np
 
 # import DNest4
@@ -112,7 +111,7 @@ logZdnest4, infogaindnest4, _ = dnest4.postprocess(plot=False);
 
 postsamples = np.loadtxt('posterior_sample.txt')
 
-print(six.u('Marginalised evidence is {}'.format(logZdnest4)))
+print('Marginalised evidence is {}'.format(logZdnest4))
 
 print('Number of posterior samples is {}'.format(postsamples.shape[0]))
 

content/downloads/code/test_PyPolyChord.py
@@ -9,7 +9,6 @@ from __future__ import print_function, division
 
 import os
 import sys
-import six
 from scipy.special import ndtri
 import numpy as np
 import resource
@@ -113,7 +112,7 @@ output = PyPolyChord.run_polychord(loglikelihood_polychord, ndims, nderived, set
 resource.setrlimit(resource.RLIMIT_STACK, curlimit)
 
 # output marginal likelihood
-print(six.u('Marginalised evidence is {} \u00B1 {}'.format(output.logZ, output.logZerr)))
+print('Marginalised evidence is {} ± {}'.format(output.logZ, output.logZerr))
 
 # plot posterior samples (if corner.py is installed)
 try:
@@ -131,4 +130,3 @@ print('Number of posterior samples is {}'.format(postsamples.shape[0]))
 
 fig = corner.corner(postsamples, labels=[r"$m$", r"$c$"], truths=[m, c])
 fig.savefig('PyPolyChord.png')
-

content/downloads/code/test_UltraNest.py
@@ -9,7 +9,6 @@ from __future__ import print_function, division
 
 import os
 import sys
-import six
 from scipy.special import ndtri
 import numpy as np
 
@@ -102,7 +101,7 @@ logZultranest = result['logZ']        # value of logZ
 logZerrultranest = result['logZerr']  # estimate of the statistcal uncertainty on logZ
 
 # output marginal likelihood
-print(six.u('Marginalised evidence is {} \u00B1 {}'.format(logZultranest, logZerrultranest)))
+print('Marginalised evidence is {} ± {}'.format(logZultranest, logZerrultranest))
 
 # get the posterior samples (first output is samples in the unit hypercube, so ignore that)
 nsamples = np.array([xi for ui, xi, Li, logwidth in result['weights']])
@@ -118,4 +117,3 @@ print('Number of posterior samples is {}'.format(postsamples.shape[0]))
 if doplot:
     fig = corner.corner(postsamples, labels=[r"$m$", r"$c$"], truths=[m, c])
     fig.savefig('UltraNest.png')
-